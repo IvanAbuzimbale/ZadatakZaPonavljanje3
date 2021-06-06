27ca1486297affb54481f371db2af7bee18e17ed
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-06-06 16:00:19 +0200

    Add files via upload

---
+++ ZadatakZaPonavljanje3/ZadatakZaPonavljanje3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakZaPonavljanje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iznos = 0;
+            Console.WriteLine("Upišite neki broj: ");
+            iznos = Convert.ToInt32(Console.ReadLine());
+            if (iznos % 4 == 0 && iznos % 6 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv sa 4 i 6.");
+            }
+            else if (iznos % 4 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv sa 4.");
+            }
+            else if (iznos % 6 == 0)
+            {
+                Console.WriteLine("Broj je djeljiv sa 6.");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije djeljiv sa 4 niti 6.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
